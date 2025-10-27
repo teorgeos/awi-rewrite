f1bc9d7907bcddbceb3d5bc504fa887463d4c522
Hyebin Seo <teorgeos@gmail.com>
2025-10-27 18:49:51 +0900

    changed some texts

--- script.js
+++ script.js
@@ -160,28 +160,3 @@ $("#flipbook").bind("turned", function (e, page) {
 console.log("초기 현재 페이지:", $("#flipbook").turn("page"));
 
 
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-  const pageMap = {
-    4: "img/8.png",
-    6: "img/1.png",
-    8: "img/4.png"
-  };
-
-  Object.entries(pageMap).forEach(([pageNum, newImgSrc]) => {
-    const page = document.querySelector(`.page${pageNum}`);
-    if (!page) return;
-
-    const firstImg = page.querySelector(".hintnumber img:first-child");
-    if (!firstImg) return;
-
-    firstImg.addEventListener("click", () => {
-      firstImg.style.opacity = "0";
-      setTimeout(() => {
-        firstImg.src = newImgSrc;
-        firstImg.style.opacity = "1";
-      }, 600);
-    });
-  });
-});
-</script>
